7dd9abaa4f67ff7736ad81c6cb434ea479bd473b
Shashank Kumar <skrb7f16@gmail.com>
2020-09-28 11:43:23 +0530

    ui made better

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from tkinter import *
 from tkinter import messagebox
 import requests, json
 type='sports'
-apiKey= 'YOUR API KEY KEY HERE'
+apiKey= '5b9ff0bf246741389fee11e0a2cb6245'
+BASE_URL=f'http://newsapi.org/v2/top-headlines?country=in&category={type}&apiKey='+apiKey
 
 
 
@@ -18,11 +19,13 @@ class NewsApp:
         self.newsCat=["genral","entertainment","business","sports","technology","health"]
 
         #========title frame===========#
-        bg_color = "#074463"
-        title = Label(self.root, text="NewsPaper Software", font=("times new roman", 30,"bold"), pady=2, bd=12, relief=GROOVE, bg=bg_color, fg="white").pack(fill=X)
+        bg_color = "#404040"
+        text_area_bg="#b8e0c4"
+        basic_font_color="#ccc4c4"
+        title = Label(self.root, text="NewsPaper Software", font=("times new roman", 30,"bold"), pady=2, bd=12, relief=GROOVE, bg=bg_color, fg=basic_font_color).pack(fill=X)
 
         #=======First frame======#
-        F1=LabelFrame(self.root,text="Category",font=("times new roman",20,"bold"),bg=bg_color,fg="gold",bd=10,relief=GROOVE)
+        F1=LabelFrame(self.root,text="Category",font=("times new roman",20,"bold"),bg=bg_color,fg=basic_font_color,bd=10,relief=GROOVE)
         F1.place(x=0,y=80,width=300,relheight=0.88)
 
         for i in range(len(self.newsCat)):
@@ -34,10 +37,10 @@ class NewsApp:
 
         #=======news frame=======#
         F2=Frame(self.root,bd=7,relief=GROOVE)
-        F2.place(x=320,y=80,relwidth=0.7, height=580)
+        F2.place(x=320,y=80,relwidth=0.7, relheight=0.8)
         news_title=Label(F2,text="News Area",font=("arial",20,"bold"),bd=7,relief=GROOVE).pack(fill=X)
         scroll_y=Scrollbar(F2,orient=VERTICAL)
-        self.txtarea=Text(F2,yscrollcommand=scroll_y.set,font=("times new roman",15,"bold"))
+        self.txtarea=Text(F2,yscrollcommand=scroll_y.set,font=("times new roman",15,"bold"),bg=text_area_bg,fg="#3206b8")
         scroll_y.pack(side=RIGHT,fill=Y)
         scroll_y.config(command=self.txtarea.yview)
         self.txtarea.insert(END,"PLEASE SELECT ANY CATEGORY TO SHOW HEADLINES AND PLEASE BE PATIENT IT DEPENDS ON UR INTERNET CONNECTIONS!!!!")
@@ -51,15 +54,19 @@ class NewsApp:
         self.txtarea.insert(END,"--------------------------------------------------------------------\n")
         try:
             articles=(requests.get(BASE_URL).json())['articles']
-            for i in range(5):
-                self.txtarea.insert(END,f"{articles[i]['title']}\n")
-                self.txtarea.insert(END,f"{articles[i]['description']}\n\n")
-                self.txtarea.insert(END,f"{articles[i]['content']}\n\n")
-                self.txtarea.insert(END,f"read more...{articles[i]['url']}\n")
-                self.txtarea.insert(END,"--------------------------------------------------------------------\n")
-                self.txtarea.insert(END,"--------------------------------------------------------------------\n")
+            if(articles!=0):
+                for i in range(len(articles)):
+                    self.txtarea.insert(END,f"{articles[i]['title']}\n")
+                    self.txtarea.insert(END,f"{articles[i]['description']}\n\n")
+                    self.txtarea.insert(END,f"{articles[i]['content']}\n\n")
+                    self.txtarea.insert(END,f"read more...{articles[i]['url']}\n")
+                    self.txtarea.insert(END,"--------------------------------------------------------------------\n")
+                    self.txtarea.insert(END,"--------------------------------------------------------------------\n")
+            else:
+                self.txtarea.insert(END,"Sorry no news available")
         except Exception as e:
-            messagebox.showerror('ERROR',e)
+            messagebox.showerror('ERROR',"Sorry cant connect to internet or some issues with newsapp :'(")
+
 
 
 
